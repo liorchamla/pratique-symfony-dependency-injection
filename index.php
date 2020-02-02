7ec48101e1cb768e8f3b2e9d55864ee6a639dcf3
Lior Chamla <lchamla@gmail.com>
2020-02-02 19:10:33 +0100

    Service definitions

--- index.php
+++ index.php
@@ -4,19 +4,33 @@ use App\Controller\OrderController;
 use App\Database\Database;
 use App\Mailer\GmailMailer;
 use App\Texter\SmsTexter;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$database = new Database();
-$texter = new SmsTexter("service.sms.com", "apikey123");
-$mailer = new GmailMailer("lior@gmail.com", "123456");
-$controller = new OrderController($database, $mailer, $texter);
+$container = new ContainerBuilder();
+
+$databaseDefinition = new Definition(Database::class);
+$container->setDefinition('database', $databaseDefinition);
+// $database = new Database();
+
+$texterDefinition = new Definition(SmsTexter::class);
+$texterDefinition->setArguments(["service.sms.com", "apikey123"]);
+$container->setDefinition('texter.sms', $texterDefinition);
+// $texter = new SmsTexter("service.sms.com", "apikey123");
+
+$mailerDefinition = new Definition(GmailMailer::class, ["lior@gmail.com", "123456"]);
+$container->setDefinition('mailer.gmail', $mailerDefinition);
+// $mailer = new GmailMailer("lior@gmail.com", "123456");
+
+$controller = new OrderController($container->get('database'), $container->get('mailer.gmail'), $container->get('texter.sms'));
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 
-if($httpMethod === 'POST') {
+if ($httpMethod === 'POST') {
     $controller->placeOrder();
     return;
 }
 
-include __DIR__. '/views/form.html.php';
+include __DIR__ . '/views/form.html.php';
